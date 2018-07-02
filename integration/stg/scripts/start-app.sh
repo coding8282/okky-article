2f2cc3e7b819d1896ba4007e18ba45383512e0c0
hyunsu <coding8282@gmail.com>
2018-07-03 03:50:36 +0900

    배포스크립트를 OS service를 이용하도록 켜고 끄도록 수정

--- integration/stg/scripts/start-app.sh
+++ integration/stg/scripts/start-app.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-nohup java -jar \
-/home/ec2-user/okky-article-1.0.0.jar \
---spring.cloud.config.label=develop \
-> /dev/null 2> /dev/null < /dev/null &
-
-sleep 20s
+sudo chmod +x /home/ec2-user/okky-article-1.0.0.jar
+sudo ln -sf /home/ec2-user/okky-article-1.0.0.jar /etc/init.d/okky-article
+sudo service okky-article start
+sleep 10s
