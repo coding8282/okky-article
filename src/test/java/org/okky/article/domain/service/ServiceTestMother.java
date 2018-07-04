e0d9e69c662a0402220e52491ec4ff0853666fb2
hyunsu <coding8282@gmail.com>
2018-07-05 02:22:43 +0900

    스크랩 응용서비스 로직에 이벤트 발령 코드 추가 및 단위테스트 보강
    - powermockito 도입

--- src/test/java/org/okky/article/domain/service/ServiceTestMother.java
+++ src/test/java/org/okky/article/domain/service/ServiceTestMother.java
@@ -1,9 +1,18 @@
 package org.okky.article.domain.service;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.okky.article.TestMother;
+import org.okky.article.domain.event.DomainEventPublisher;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(DomainEventPublisher.class)
 public abstract class ServiceTestMother extends TestMother {
+    @Before
+    public void setUp() {
+        PowerMockito.mockStatic(DomainEventPublisher.class);
+    }
 }
